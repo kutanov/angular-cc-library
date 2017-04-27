520d24e0170303343e8220f3a07be07c15079f77
Roman Kutanov <kutanov@gmail.com>
2017-04-27 16:02:23 +0200

    fix

--- dist/credit-card.validator.d.ts
+++
@@ -1,5 +0,0 @@
-import { AbstractControl } from '@angular/forms';
-export declare class CreditCardValidator {
-    static validateCCNumber(control: AbstractControl): any;
-    static validateExpDate(control: AbstractControl): any;
-}
